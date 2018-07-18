41535356f209b698e9124bc3c6456cd35efe5903
Balthasar <rileysprice@gmail.com>
2018-07-18 13:18:41 -0600

    Primitive Data Types

---
+++ variables/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myNumber = 13;
+        short myShort = 1313;
+        long myLong = 1313131313;
+        double myDouble = 12.43554354352342345;
+        float myBoat = 3.45f;
+        char myChar = 'R';
+        boolean myBool = true;
+        byte me = 127;
+
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myBoat);
+        System.out.println(myChar);
+        System.out.println(myBool);
+        System.out.println(me   );
+
+
+
+
+    }
+}
