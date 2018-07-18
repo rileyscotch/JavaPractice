7b670cce3040c99b1174f58c9f176b215c12f24e
Balthasar <rileysprice@gmail.com>
2018-07-18 13:41:22 -0600

    Stringy

---
+++ strings/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String text = "Ciao Bella";
+
+        String blank = " ";
+
+        String name = "Coleman";
+
+        String sentence = text + blank + name;
+
+        System.out.println(sentence);
+
+    }
+
+}
