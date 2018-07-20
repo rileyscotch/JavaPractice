41f6d8b6c9b4cd1cb5dcef3b490bf6a0c4ed1ae7
Balthasar <rileysprice@gmail.com>
2018-07-20 13:00:00 -0600

    Conditionals

---
+++ ifstatemens/src/Application.java
@@ -0,0 +1,17 @@
+public class Application {
+    public static void main(String[] args) {
+
+        int loop = 0;
+
+        while(true) {
+            System.out.println("Looping " + loop);
+
+            if(loop == 5) {
+                break;
+            }
+                loop++;
+            System.out.println("Running");
+
+        }
+    }
+}
